d1cff89f0dde293967757e29a5e60314979de18f
Paras Mogla <paras@getswan.co>
2024-03-14 16:03:15 +0530

    version change

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
-import dotenv from "dotenv";
-export const { FILE_UPLOAD_URL } = dotenv.config().parsed;
+// import dotenv from "dotenv";
+// export const { FILE_UPLOAD_URL } = dotenv.config().parsed;
+export const FILE_UPLOAD_URL = "https://9xc8mxwlkj.execute-api.us-east-1.amazonaws.com/staging";
 
 export const UPPY_FILE_UPLOAD_ENDPOINT = {
   UPLOAD_START: "/upload/start",
